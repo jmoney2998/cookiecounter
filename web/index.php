60fd5442bbd55945f596f9ea4c38083752124451
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2017-11-17 21:11:11 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@ $archivo = "conta.txt"; // n visitatori
 if(file_exists($archivo)){
   $conta = file_get_contents($archivo);
   $cook= $_COOKIE[‘cook’]; // leggo cookie
-    if (!$cook)
+    if ($cook)
        $conta ++; // // se non esiste incremento le visite
 }
 else{
@@ -13,6 +13,6 @@ else{
   $conta = 1;
 }
 setcookie("cook",0, time() + 3600);
-//file_put_contents($archivo, $conta);
+file_put_contents($archivo, $conta);
 echo $conta;
 ?>
