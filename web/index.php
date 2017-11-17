a5467c2816004e684916ad4b76a94b11840a5711
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2017-11-17 20:40:50 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,16 +1,18 @@
 <?php
 $archivo = "conta.txt"; // n visitatori
 if(file_exists($archivo)){
-$conta = file_get_contents($archivo);
-$cook= $_COOKIE[‘cook’]; // leggo cookie
-if (!$cook)$conta ++; // // se non esiste ncremento le visite
-}else{
-// creo l'archivio (SOLO LA PRIMA VOLTA)
-touch($archivo);
-chmod($archivo, 755);
-$conta = 1;
+  $conta = file_get_contents($archivo);
+  $cook= $_COOKIE[‘cook’]; // leggo cookie
+    if (!$cook)
+       $conta ++; // // se non esiste ncremento le visite
+}
+else{
+// creo l'archivio (SOLO LA PRIMA VOLTA)  
+  touch($archivo);
+  chmod($archivo, 755);
+  $conta = 1;
 }
 setcookie("cook",1, time() + 3600);
-file_put_contents($archivo, $conta);
+//file_put_contents($archivo, $conta);
 echo $conta
 ?>
